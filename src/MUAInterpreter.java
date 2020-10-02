e354195e7e1a0de7d848450bcb19179bd54b5a0d
Steven <3180101968@zju.edu.cn>
2020-10-02 18:54:39 +0800

    [WIP] Rewrite Tokenizing in Interpreter

--- src/MUAInterpreter.java
+++ src/MUAInterpreter.java
@@ -3,38 +3,74 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class MUAInterpreter {
-    private static Map<String, Operation> opMap
-    = Map.of(
-    "make",     Operation.MAKE,
-    "thing",    Operation.THING,
-    "print",    Operation.PRINT,
-    "read",     Operation.READ,
-    "add",      Operation.ADD,
-    "sub",      Operation.SUB,
-    "mul",      Operation.MUL,
-    "div",      Operation.DIV,
-    "mod",      Operation.MOD
-    );
     private static Pattern numberPattern = Pattern.compile("-?\\d+\\.?\\d*");
-    private static Pattern listPattern = Pattern.compile("\\[\\s*\\w+");
-    private static Pattern wordPattern = Pattern.compile("\"\\w+");
-    private static Pattern commaPattern = Pattern.compile(":\\w+");
-    private static Pattern namePattern = Pattern.compile("[a-zA-Z]\\w*");
+    private static Pattern listPattern = Pattern.compile("\\[\\s*.+");
+    private static Pattern exprPattern = Pattern.compile("\\(\\s*.+");
+    private static Pattern wordPattern = Pattern.compile("\".+");
+    private static Pattern commaPattern = Pattern.compile(":\\.+");
+    private static Pattern namePattern = Pattern.compile("[a-zA-Z]\\[a-zA-Z0-9_]*");
     private static Pattern boolPattern = Pattern.compile("true|false");
     private Scanner in = new Scanner("");
     public MUAInterpreter (Scanner in) {
         this.in = in;
     }
     public String nextToken () {
-        return in.next();
+        // get the next complete token
+        // especially for token begins with [ and ( 
+        String input = in.next();
+        if (listPattern.matcher(input).matches()) {
+            return nextListToken(input);
+        } else if (exprPattern.matcher(input).matches()) {
+            return nextExprToken(input);
+        } else {
+            return input;
+        }
+    }
+    public String nextListToken(String input) {
+        String result = input;
+        String token;
+        int index = 1;
+        int leftMinusRight = 1; // the number of [ minus the number of ]
+        // if a token begins with ", we need to skip it
+        while (leftMinusRight > 0) {
+            // if off the result's length, read more
+            if (index >= result.length()) {
+                while ((token = in.next()).charAt(0) == '\"') {
+                    // concatenate
+                    result = String.format("%s %s", result, token);
+                    index = result.length();
+                }
+                result = String.format("%s %s", result, token);
+            }
+            leftMinusRight += (result.charAt(index) == '[') ? 1 : 0;
+            leftMinusRight -= (result.charAt(index) == ']') ? 1 : 0;
+            index++;
+        }
+        return result;
+    }
+    public String nextExprToken(String input) {
+        String result = input;
+        int index = 1;
+        int leftMinusRight = 1; // the number of [ minus the number of ]
+        while (leftMinusRight > 0) {
+            // if off the result's length, read more
+            if(index >= result.length())
+                result = String.format("%s %s", result, in.next());
+            leftMinusRight += (result.charAt(index) == '(') ? 1 : 0;
+            leftMinusRight -= (result.charAt(index) == ')') ? 1 : 0;
+            index++;
+        }
+        return result;
     }
     public Operation interpretOperation (String token) {
         // 1. if it is inner operation, return type(see Operation class)
         // 2. if it has number/bool/list/word pattern, return Operation.VALUE
         // 3. otherwise, check whether it can be a name, if yes, return Operation.NAME
         Operation op = Operation.UNKNOWN;
-        if (opMap.containsKey(token)) {
-            op = opMap.get(token);
+        if (Operation.opMap.containsKey(token)) {
+            op = Operation.opMap.get(token);
+        } else if(commaPattern.matcher(token).matches()) {
+
         } else if(isValueLiteral(token)) {
             op = Operation.VALUE;
         } else if(namePattern.matcher(token).matches()){
@@ -42,93 +78,21 @@ public class MUAInterpreter {
         } // else UNKNOWN
         return op;
     }   
-    public Value nextValue() {
-        Value result = null;
-        if (in.hasNext(numberPattern)) {
-            result = nextNumber();
-        } else if (in.hasNext(listPattern)) {
-            result = nextList();
-        } else if (in.hasNext(wordPattern)) {
-            result = nextWord();
-        } else if(in.hasNext(boolPattern)) {
-            result = nextBool();
-        } else {
-            // reading an operation
-            
-        }
-        return result;
-    }
-    public Name nextName() {
-        Name result = null;
-        String token = in.next();
-        // BUG: only check whether the name is in namespace
-        // let executor do the checking
-        if (namePattern.matcher(token).matches()) {
-            result = Name.newInstance(token);
-        } else {
-            // throw exception for no such name
-        }
-        return result;
-    }
-    public Word nextWord() {
-        Word result = null;
-        String token = in.next();
-        if (wordPattern.matcher(token).matches()) {
-            // remove the first \"
-            result = Word.newInstance(token.substring(1));
-        } else {
-            
-
-        }
-        return result;
-    }
-    public List nextList() {
-        List result = null;
-        String content = String.valueOf("");
-        String token = in.next();
-        if (token.startsWith("[")) {
-            int stack = 1;
-            while (stack != 0) {
-                content = content.concat(token + " ");
-                token = in.next();
-                if (token.endsWith("]")) {
-                    stack --;
-                }
-                if (token.startsWith("[")) {
-                    stack ++;
-                }
-            }
-            result = List.newInstance
-                    (content.substring(1, content.length()-1));
-        } else {
-            
-        }
-        return result;
-    }
-    public Number nextNumber() {
-        Number result = null;
-        String token = in.next();
-        if (numberPattern.matcher(token).matches()) {
-            result = Number.newInstance(token);
-        } else {
-            
-        }
-        return result;
-    }
-    public Bool nextBool() {
-        Bool result = null;
-        String token = in.next();
-        if (boolPattern.matcher(token).matches()) {
-            result = Bool.newInstance(token);
-        } else {
-            
-        }
-        return result;
-    }
     private boolean isValueLiteral (String input) {
         return  listPattern.matcher(input).matches()
             ||  wordPattern.matcher(input).matches()
             ||  numberPattern.matcher(input).matches()
             ||  boolPattern.matcher(input).matches();
     }
+
+    // Unit Test 
+    public static void main (String[] args) {
+        String input = "hello 2344 \"sdfe (2+3 / thing \"a + 9) [print \"you[sucks aloha ba 134245] [[[you]]]";
+        Scanner in = new Scanner(input);
+        MUAInterpreter interpreter = new MUAInterpreter(in);
+        while (in.hasNext()) {
+            System.out.println("Token: " + interpreter.nextToken());
+        }
+    }
+
 }
